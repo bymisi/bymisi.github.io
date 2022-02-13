e15ad97600986cfaabb343a279484a1c2d242de2
misi-c <misi@dr.com>
2022-02-13 23:37:03 +0100

    instandblack

--- 1TermuxFiles/BlacklistCreator.sh
+++ 1TermuxFiles/BlacklistCreator.sh
@@ -14,18 +14,40 @@ echo ' \n--------------------\nCreating blacklist.txt\n--------------------\n' &
 
 sed '/Ads/,$!d' d3host.txt >blacklist.txt ;
 
+cat blacklist.txt;
+echo "-------------------------";
+echo "1. Deleted everything before (Ads)"
+echo "-------------------------";
+sleep 1;
 sed -i 's/0.0.0.0 //g' blacklist.txt ;
 
+cat blacklist.txt;
+
+echo "-------------------------";
+echo "2. cleared start of all lines (0.0.0.0.)";
+echo "-------------------------";
+sleep 1;
 sed -i '/#/d' blacklist.txt ;
 
+cat blacklist.txt;
+
+echo "-------------------------";
+echo "3. deleted all lines containing #";
+echo "-------------------------";
+sleep 1;
 sed -i '/^[[:space:]]*$/d' blacklist.txt ;
 
 cat blacklist.txt ;
 
+echo "-------------------------";
+echo "4. removed empty lines"
+echo "-------------------------";
+sleep 1;
 echo "--------------------" && awk 'END {print NR}' blacklist.txt && echo "sites from d3ward\n--------------------" ;
 
 echo "# original file is d3ward.txt" ;
 
 echo "# edited & exported to blacklist.txt" ;
-
-echo "# 丂匚尺丨 卩ㄒ 匚尺乇卂ㄒ乇ᗪ 🇧‌🇾‌🇲‌🇮‌🇸‌🇮" ;
+sleep 1;
+echo -n "# 丂匚尺丨卩ㄒ";sleep 1 && echo " 匚尺乇卂ㄒ乇ᗪ" ;
+echo -n " 🇧‌"; sleep 1 && echo -n "🇾‌"; sleep 1 && echo "🇲‌🇮‌🇸‌🇮"; sleep 1;
--- 1TermuxFiles/InstalledCreator.sh
+++ 1TermuxFiles/InstalledCreator.sh
@@ -1,38 +1,70 @@
 rm pkglist.sh ;
 clear ;
-echo ' \n-----------------------\nGetting installed List \n-----------------------\n' && sleep 2 ;
 
 pkg list-installed >pkglist.sh ;
 
-cat pkglist.sh && sleep 3 ;
+cat pkglist.sh ;
 
-echo ' \n---------------------\nExtracting pkg names\n---------------------\n' && sleep 2 ;
+echo ' \n-----------------------\n List installed Packages  \n-----------------------\n' ;
+sleep 3;
 
+echo "--------------------";
+echo " 1. Remove first line from file (Listing ...)";
+echo "--------------------";
+sleep 2;
 gawk -i inplace 'NR>1' pkglist.sh ;
+cat pkglist.sh;
 
+echo "--------------------";
+echo " 2. Remove content from all lines after /  " ;
+echo "--------------------";
+sleep 2;
 sed -i 's/\/.*//' pkglist.sh ;
+cat pkglist.sh ;
+echo ' \n---------------------\nExtracted pkg names\n---------------------\n' ;
 
-cat pkglist.sh && sleep 3 ;
-
-echo ' \n-------------------------\nCreating autoinstall file\n-------------------------\n' && sleep 2 ;
-
-
+sleep 3 ;
+echo "--------------------";
+echo "3. add to begining of all lines (;apt satisfy -y) "
+echo "--------------------";
+sleep 2;
 sed -i 's/^/;apt satisfy -y /' pkglist.sh ;
+cat pkglist.sh;
 
+echo "--------------------";
+echo "4. add numbering to all lines front "
+echo "--------------------";
+sleep 2;
 gawk -i inplace '{print NR, $0}' pkglist.sh ;
+cat pkglist.sh ;
 
+echo "--------------------";
+echo "5. add content to begining of all lines (# ✓ )";
+echo "--------------------";
+sleep 2;
 sed -i 's/^/# ✓ /' pkglist.sh ;
+cat pkglist.sh;
 
+echo "--------------------";
+echo "6. add split to all lines before match (apt sat) "
+echo "--------------------";
+sleep 2;
 sed -i 's/apt sat/\n&/g' pkglist.sh ;
+cat pkglist.sh;
 
-echo "# --------------------------">>pkglist.sh ;
-
-echo "# autoinstall: sh pkglist.sh">>pkglist.sh ;
-
-echo "# --------------------------">>pkglist.sh ;
-
-echo "# 丂匚尺丨 卩ㄒ 匚尺乇卂ㄒ乇ᗪ 🇧‌🇾‌🇲‌🇮‌🇸‌🇮">>pkglist.sh ;
-
+echo "--------------------";
+echo "7. add 1.st line to file to exclude error (apt update) "
+echo "--------------------";
+sleep 2;
 sed -i '1s;^;apt update\n;' pkglist.sh ;
 
+echo "# --------------------------">>pkglist.sh ;
+echo "# autoinstall: sh pkglist.sh">>pkglist.sh ;
+echo "# --------------------------">>pkglist.sh ;
+
 cat pkglist.sh
+
+echo ' \n-------------------------\nCreated autoinstall file\n-------------------------\n' ;
+sleep 1;
+echo -n "# 丂匚尺丨卩ㄒ";sleep 1 && echo " 匚尺乇卂ㄒ乇ᗪ" ;
+echo -n " 🇧‌"; sleep 1 && echo -n "🇾‌"; sleep 1 && echo " 🇲‌🇮‌🇸‌🇮"; sleep 1;
